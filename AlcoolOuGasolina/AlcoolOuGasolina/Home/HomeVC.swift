9207c7c144412c1d9bdc3810822bce22c994f07a
Vitor Ernane <vitorernane@hotmail.com>
2023-03-04 23:22:57 -0300

    Nesse comit foi configurado a Screen da home, sendo inserido as constraints e a imagem que irá compor o background, também foram inseridos os assets do projeto

--- AlcoolOuGasolina/AlcoolOuGasolina/Home/HomeVC.swift
+++ AlcoolOuGasolina/AlcoolOuGasolina/Home/HomeVC.swift
@@ -8,11 +8,17 @@
 import UIKit
 
 class HomeVC: UIViewController {
+    
+    var screen: HomeScreen?
+    
+    override func loadView() {
+        screen = HomeScreen()
+        view = screen
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        view.backgroundColor = .black
     }
 
 
--- AlcoolOuGasolina/AlcoolOuGasolina/Home/Screen/HomeScreen.swift
+++ AlcoolOuGasolina/AlcoolOuGasolina/Home/Screen/HomeScreen.swift
@@ -8,7 +8,45 @@
 import UIKit
 
 class HomeScreen: UIView {
-
     
+    lazy var backgroundImageView: UIImageView = {
+        let image = UIImageView()
+        /* isso serve para que o xcode respeite as constraints que forem setadas manualmente */
+        image.translatesAutoresizingMaskIntoConstraints = false
+        /* aqui ele está buscando dentro do arquivo a imagem como nome BG HOME para inserir ela no background */
+        image.image = UIImage(named: "BG HOME")
+        /* contentMode é a forma em que a imagem irá se enquadrar na tela, no caso foi selecionado o AspectFit */
+        image.contentMode = .scaleAspectFit
+        return image
+    }()
 
+    /* Para inicializar é só digitar o init e selecionar o que tem frame no campo de seleção */
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        /* aqui você está adicionando uma subview na viewController, avisando que agora ela existe e faz parte do projeto */
+        addSubview(backgroundImageView)
+        configConstraints()
+    }
+    /* aqui o Xcode já reporta o erro com um Fix, selecione-o e já irá aparecer tudo digitado */
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    
+    /* Configurando as constraints da tela */
+    private func configConstraints() {
+        NSLayoutConstraint.activate([
+            
+        backgroundImageView.topAnchor.constraint(equalTo: topAnchor),
+        backgroundImageView.leadingAnchor.constraint(equalTo: leadingAnchor),
+        backgroundImageView.trailingAnchor.constraint(equalTo: trailingAnchor),
+        backgroundImageView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        
+        
+        ])
+    }
+    
 }
